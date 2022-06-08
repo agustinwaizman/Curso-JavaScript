0083ffe2176d17b66530cbbd85911ea3314ae488
Agustin <105565517+agustinwaizman@users.noreply.github.com>
2022-06-08 13:44:37 -0300

    Add files via upload

---
+++ capitulo-2/tarea1.js
@@ -0,0 +1,9 @@
+const cumpleaños = new Date(1998, 5, 20)
+
+const arr = ["Agustin, Waizman", 24, true, cumpleaños,{
+    titulo:"el hobbit",
+    autor:"JRR Tolkien",
+    año:"1937",
+    url:"https://g.co/kgs/C5ZLHn"
+}]
+console.log(arr)
