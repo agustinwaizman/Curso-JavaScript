c20b0706878d1dea729d0b46e20ffcf23ecdb81f
Agustin <105565517+agustinwaizman@users.noreply.github.com>
2022-06-16 12:38:20 -0300

    Add files via upload

---
+++ sesion-13/eslint-npm/index.js
@@ -0,0 +1,14 @@
+// main archivo
+const nombre = "Agustin"
+
+/* esLint-disable */
+
+const persona2 = 'Maria'
+
+/* esLint-enable */
+
+// en esta linea quiero tener comillas simples(quiero que desactive la regla de las comillas dobles)
+const nuevoString = 'Esto es un string'; //esLint-disable-line
+
+/* esLint-disable-next-line indent */
+const string2 = 'Mas strings'
---
+++ sesion-13/index.js
@@ -0,0 +1,9 @@
+const nombre = "Agustin";
+
+var nombre2 = 'pedro';
+
+let objeto = {
+    nombre: "Circulo",
+    radio: 2,
+    nombre: "Rectangulo"
+}
